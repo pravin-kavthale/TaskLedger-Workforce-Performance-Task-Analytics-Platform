f30473c3cd968372040fba6299290b2d4a1b044d
pravin-kavthale <kavthalepravin@gmail.com>
2026-01-31 15:39:29 +0530

    add CreateUser endpoint to URL patterns for user creation

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 
-from .views import CustomTokenObtainPairView,ProtectedTestView
+from .views import CreateUserView, CustomTokenObtainPairView,ProtectedTestView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -12,4 +12,5 @@ urlpatterns = [
     path('auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/protected/', ProtectedTestView.as_view(), name='protected_test'),
+    path('auth/create-user/', CreateUserView.as_view(), name='create_user'),
 ]
