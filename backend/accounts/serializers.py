54ab49d4918a798fc92a244e7505817129341e62
pravin-kavthale <kavthalepravin@gmail.com>
2026-01-31 15:35:26 +0530

    add CreateUserSerializer for user creation and role validation

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
+from rest_framework import serializers
+from .models import User
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
@@ -13,3 +14,22 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['email'] = user.email
 
         return token
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id','username','email','role','password']
+        extra_kwargs = {"password": {"write_only": True}}
+
+        def vaidate_role(self,value):
+            request_user = self.context['request'].user
+            if request_user.role == 'admin':
+                return value
+            if request_user.role == 'manager' and value == 'employee':
+                return value
+            
+            raise serializers.ValidationError("You cannot assign this role")
+            
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
