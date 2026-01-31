d0aa32bff4bca47a28304ed4264b8d2dd00ae3f8
pravin-kavthale <kavthalepravin@gmail.com>
2026-01-31 15:37:49 +0530

    add CreateUserView for user creation with admin and manager permissions

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from .permissions import IsAdmin, IsAdminOrManager
+from .serializers import CreateUserSerializer
+
+
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -20,4 +24,21 @@ class ProtectedTestView(APIView):
             "user_id": request.user.id,
             "username": request.user.username,
             "role": request.user.role
+        })
+
+class CreateUserView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminOrManager]
+    
+    def post(self, request):
+        serializer = CreateUserSerializer(
+            data=request.data,
+            context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "message": "User created successfully",
+            "user_id": user.id,
+            "role": user.role
         })
