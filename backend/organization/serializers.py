a103e40aeab2d314b2357b29fe190c9ead46050e
pravin-kavthale <kavthalepravin@gmail.com>
2026-02-02 23:13:25 +0530

    implement Department model with admin permissions and validation; restrict create and update operations to Admin users only

---
+++ backend/organization/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Department
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Department
+        fields=['id','name','code','is_activate','created_at','updated_at','created_by']
+        read_only_fields=['created_at','updated_at','created_by']
+
+    def validate(self,attrs):
+        request_user = self.context['request'].user
+        if request_user.role != "Admin":
+            raise serializers.ValidationError("Only Admin users can create a department.")
+        return attrs
+        
+    def create(self, validated_data):
+        validated_data['created_by'] = self.context['request'].user
+        department = Department.objects.create(**validated_data)
+        return department
+        
--- backend/organization/views.py
+++ backend/organization/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from . models import Department
+from . serializers import DepartmentSerializer
+from . permissions import IsAdminUser 
+from rest_framework.exceptions import MethodNotAllowed
 
-# Create your views here.
+
+class DepartmentViewSet(
+    mixins.ListModelMixin,    # GET /departments/
+    mixins.RetrieveModelMixin,# GET /departments/{id}/
+    mixins.CreateModelMixin,  # POST /departments/
+    viewsets.GenericViewSet
+):
+    serializer_class = DepartmentSerializer
+    queryset = Department.objects.all()
+    permission_classes = [IsAdminUser]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+    
+    def partial_update(self, request, *args, **kwargs):
+        # Allow patch only for 'is_activate' field
+        instance = self.get_object()
+        if 'is_activate' not in request.data:
+            raise MethodNotAllowed(request.method, detail="Only 'is_activate' can be updated.")
+        return super().partial_update(request, *args, **kwargs)
+    
+    # Disable PUT and DELETE methods
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method, detail="Full update is not allowed. Use PATCH to update 'is_activate' only.")
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed(request.method, detail="Delete operation is not allowed.")
