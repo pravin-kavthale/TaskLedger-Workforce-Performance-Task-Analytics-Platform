7e42f4c67860c0568c46476b77bd1b64494236c5
pravin-kavthale <kavthalepravin@gmail.com>
2026-02-12 15:23:57 +0530

    feat: add Team model with fields for name, code, department, manager, and members; implement created_at and is_activate fields

--- backend/organization/models.py
+++ backend/organization/models.py
@@ -1,6 +1,8 @@
 # departments,teams,structures
 from django.db import models
 
+from accounts.models import User
+
 class Department(models.Model):
     name=models.CharField(max_length=100,unique=True)
     code=models.CharField(max_length=20,unique=True)
@@ -17,4 +19,31 @@ class Department(models.Model):
 
     def __str__(self):
         return self.name
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=20, unique=True)
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        related_name="teams"
+    )
+
+    manager = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="managed_teams"
+    )
+
+    members = models.ManyToManyField(
+        User,
+        related_name="teams"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_activate = models.BooleanField(default=True)
+
+
+
     
