7cf7c05b08e9a8df7e7db458640797e4bf47848c
pravin-kavthale <kavthalepravin@gmail.com>
2026-03-16 11:33:40 +0530

    feat: add GitHub OAuth configuration and implement authorization view

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -179,3 +179,16 @@ SIMPLE_JWT = {
     
 }
 
+# ---------------------------
+# GITHUB OAUTH CONFIGURATION
+# ---------------------------
+
+
+
+GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+GITHUB_REDIRECT_URI = os.getenv("GITHUB_REDIRECT_URI")
+
+
+
+
--- backend/integrations/github/views.py
+++ backend/integrations/github/views.py
@@ -1,13 +1,31 @@
+import secrets
+from urllib.parse import urlencode
+
+from django.conf import settings
+from django.shortcuts import redirect
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
 
-from .services import GitHubService
+class GitHubAuthorizationView(APIView):
 
-
-class GitHubCallbackView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        pass
+
+        state = secrets.token_urlsafe(16)
+
+        request.session["github_oauth_state"] = state
+
+        params = {
+            "client_id": settings.GITHUB_CLIENT_ID,
+            "redirect_uri": settings.GITHUB_REDIRECT_URI,
+            "scope": "repo user",
+            "state": state,
+        }
+
+        authorization_url = (
+            "https://github.com/login/oauth/authorize?"
+            + urlencode(params)
+        )
+
+        return redirect(authorization_url)
